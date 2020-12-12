cdba08adbda05c670d50fd291369e73306819124
Tav Hafner <23143092+haftav@users.noreply.github.com>
2020-12-12 14:51:26 -0500

    Add width attribute on SVG to fix safari issue

--- components/ProgressRing.tsx
+++ components/ProgressRing.tsx
@@ -15,6 +15,7 @@ const ProgressRing = ({percent = 100}: ProgressRingProps) => {
     <svg
       viewBox="0 0 100 100"
       height="100%"
+      width="100%"
       preserveAspectRatio="xMidYMid meet"
       xmlns="http://www.w3.org/2000/svg"
     >
